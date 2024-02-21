075e32af381bdd2c112ef06e4da3eb828df9b681
isabelpsanch <isabelparraehs@gmail.com>
2024-02-21 18:21:30 +0100

    Añadir ListaProductos del ejercicio2

---
+++ src/components/Ejercicio2/ListaProductos.jsx
@@ -0,0 +1,13 @@
+export default function ListaProductos(props) {
+    return (
+      <div>
+        <h3>Lista de Productos</h3>
+        <ul>
+          {props.productos.map((producto, index) => (
+            <li key={index}>{producto}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+  
