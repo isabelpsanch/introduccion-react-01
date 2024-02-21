00aece226d04b8f9c9043cfb3480bebbd730df6d
isabelpsanch <isabelparraehs@gmail.com>
2024-02-21 18:21:06 +0100

    añadir App.jsx del ejerciccio Reac/1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import Ejercicio1 from './components/Ejercicio1/Ejercicio1'
+import Ejercicio1 from './components/Ejercicio1/Saludar'
+import Ejercicio2 from './components/Ejercicio2/ListaProductos'
+import Ejercicio3 from './components/Ejercicio3/ListaProductosCondicional'
+import Ejercicio4 from './components/Ejercicio4/Contador'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TituloPrincipal from './components/TituloPrincipal'
 import './components/TituloPrincipal'
+
+
 
 export default function App() {
   return (
@@ -11,7 +16,10 @@ export default function App() {
       <main>
         <TituloPrincipal />
         <Ejercicio1 />
-        <Ejercicio1/>
+        <Ejercicio2 />
+        <Ejercicio3 />
+        <Ejercicio4 />
+        
       </main>
       <Footer />
     </>
